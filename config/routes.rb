5ff42bd4dcc9456bd07dc0b605b3d563150d8909
Christian HUANG <chrhuang@macbook-pro-de-christian.home>
2021-09-06 19:05:50 +0200

    Fix User Device

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # [...]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # devise_scope :user do
-      # post 'sign_up', to: 'registrations#create'
-      # post 'sign_in', to: 'sessions#create'
-      # end
       resources :apartments, only: %i[index show update create destroy]
     end
   end
